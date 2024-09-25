ea79fc12ed54fbc64b63e99558f969b6737e2020
aes_minhnd <balota.dev01@outlook.com>
2024-09-25 22:21:14 +0700

    dmm

--- index.js
+++ index.js
@@ -4,19 +4,13 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const cors = require('cors');
-const { chromium } = require('playwright');
+const puppeteer = require('puppeteer');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-async function installBrowsers() {
-    try {
-        await chromium.install();
-    } catch (error) {
-        console.error('Error installing browsers:', error);
-    }
-}
+
 // API để crawl dữ liệu
 app.post('/api/crawl', async (req, res) => {
     const { url } = req.body;
@@ -25,33 +19,30 @@ app.post('/api/crawl', async (req, res) => {
     }
 
     try {
-        const browser = await chromium.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
+        const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto(url, { waitUntil: 'networkidle' });
 
+        // Truy cập vào URL và chờ tải xong
+        await page.goto(url, { waitUntil: 'networkidle2' });
+
+        // Lấy văn bản từ div có class là list-group-item list-group-item-info
         const textContent = await page.evaluate(() => {
             const elements = document.querySelectorAll('.list-group-item.list-group-item-info');
             return Array.from(elements).map(element => element.innerText);
         });
 
+        // Đóng trình duyệt
         await browser.close();
 
         // Trả về nội dung văn bản
         res.send({ text: textContent });
     } catch (error) {
         console.error(error);
-        res.status(500).send({ error: error });
+        res.status(500).send({ error: 'Error crawling the URL' });
     }
 });
 
 // Khởi động server
-installBrowsers().then(() => {
-    app.listen(port, () => {
-        console.log(`Server is running at http://localhost:${port}`);
-    });
-}).catch((error) => {
-    console.error('Error starting the server:', error);
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
